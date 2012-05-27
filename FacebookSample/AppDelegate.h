a9cb2b427ba672131de96e20a270be1ce058bdb3
akifumi <akiiiiiii@pankaku.com>
2012-05-27 17:38:54 +0900

    Setup project.

---
+++ FacebookSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FacebookSample
+//
+//  Created by Fukaya Akifumi on 12/05/27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@property (strong, nonatomic) UISplitViewController *splitViewController;
+
+@end
---
+++ FacebookSample/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  FacebookSample
+//
+//  Created by Fukaya Akifumi on 12/05/27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
