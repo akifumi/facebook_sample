6ecca8dee121cbaf09cef26331e6c02d5c736400
akifumi <akiiiiiii@pankaku.com>
2012-05-27 22:32:19 +0900

    Added a button class.

--- FacebookSample/FSButton.m
+++ FacebookSample/FSButton.m
@@ -8,6 +8,41 @@
 
 #import "FSButton.h"
 
+typedef void (^ActionBlock)();
+
+@interface FSButton() {
+    ActionBlock _actionBlock;
+}
+@end
+
 @implementation FSButton
+@synthesize size = _size, position = _position;
+
+- (id)initWithFileName:(NSString *)fileName{
+    if (self = [super init]) {
+        UIImage *buttonImage = [UIImage imageNamed:fileName];
+        [self setImage:buttonImage forState:UIControlStateNormal];
+        self.size = CGSizeMake(buttonImage.size.width, buttonImage.size.height);
+        self.frame = CGRectMake(0, 0, self.size.width, self.size.height);
+    }
+    return self;
+}
+
++ (FSButton *)createWithFileName:(NSString *)fileName{
+    return [[self alloc] initWithFileName:fileName];
+}
+
+- (void)setPosition:(CGPoint)position{
+    self.frame = CGRectMake(position.x, position.y, self.frame.size.width, self.frame.size.height);
+}
+
+- (void)callActionBlock:(id)sender{
+    _actionBlock();
+}
+
+- (void)handleControlEvent:(UIControlEvents)event block:(void(^)())block{
+    _actionBlock = Block_copy(block);
+    [self addTarget:self action:@selector(callActionBlock:) forControlEvents:event];
+}
 
 @end
--- FacebookSample/FSHeader.h
+++ FacebookSample/FSHeader.h
@@ -6,9 +6,4 @@
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
-#ifndef FacebookSample_FSHeader_h
-#define FacebookSample_FSHeader_h
-
-
-
-#endif
+#import "FSButton.h"
