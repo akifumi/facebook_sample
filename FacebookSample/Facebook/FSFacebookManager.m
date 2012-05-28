9f93d7730419f5bf3479ed4e32fa0d54b6617dc8
akiii <akiii@sfc.wide.ad.jp>
2012-05-29 00:23:37 +0900

    Modified Facebook manager.

--- FacebookSample/Facebook/FSFacebookManager.m
+++ FacebookSample/Facebook/FSFacebookManager.m
@@ -8,27 +8,39 @@
 
 #import "FSFacebookManager.h"
 #import "FSFacebookObserver.h"
+#import "FSFacebookRequester.h"
+#import "FSFacebookInfo.h"
 
 static FSFacebookManager *manager = nil;
 
 @interface FSFacebookManager(){
 
 }
+@property (nonatomic, strong) Facebook *facebook;
+@property (nonatomic, strong) NSArray *permissions;
 @property (nonatomic, strong) FSFacebookObserver *observer;
+@property (nonatomic, strong) FSFacebookRequester *requester;
 @end
 
 @implementation FSFacebookManager
 @synthesize onDidLogin;
-@synthesize observer;
+@synthesize facebook, permissions, observer, requester;
 
 - (id)init{
     if (self = [super init]) {
-        self.observer = [FSFacebookObserver create];
+        self.observer = [[[FSFacebookObserver alloc] init] autorelease];
+        self.requester = [[[FSFacebookRequester alloc] init] autorelease];
+        self.facebook = [[Facebook alloc] initWithAppId:kAppId andDelegate:self.observer];
+        self.observer.facebook = self.facebook;
+        
         self.observer.onDidLogin = ^(){
+            self.observer.currentAPICall = kAPIGraphMe;
             if (self.onDidLogin) {
                 self.onDidLogin();
             }
+            [self.requester requestUserInfoWithFacebook:self.facebook observer:self.observer];
         };
+        
     }
     return self;
 }
