9c1be3bfd144948ddc9d1b8a14f30ff20b4a0282
akiii <akiii@sfc.wide.ad.jp>
2012-05-29 01:52:41 +0900

    Deleted a comment out.

--- FacebookSample/ViewController/FSHomeTableViewController.m
+++ FacebookSample/ViewController/FSHomeTableViewController.m
@@ -127,7 +127,6 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSLog(@"%d", indexPath.row);
     // Navigation logic may go here. Create and push another view controller.
     /*
      <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
