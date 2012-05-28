b5167fdd0e8e591416521fc092d8f1a09e3ca677
akiii <akiii@sfc.wide.ad.jp>
2012-05-28 23:05:01 +0900

    Added a new class and replaced classes.

---
+++ FacebookSample/Facebook/FSFacebookRequester.h
@@ -0,0 +1,13 @@
+//
+//  FSFacebookRequester.h
+//  FacebookSample
+//
+//  Created by 深谷 哲史 on 12/05/28.
+//  Copyright (c) 2012 Keio University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSFacebookRequester : NSObject
+
+@end
---
+++ FacebookSample/Facebook/FSFacebookRequester.m
@@ -0,0 +1,13 @@
+//
+//  FSFacebookRequester.m
+//  FacebookSample
+//
+//  Created by 深谷 哲史 on 12/05/28.
+//  Copyright (c) 2012 Keio University. All rights reserved.
+//
+
+#import "FSFacebookRequester.h"
+
+@implementation FSFacebookRequester
+
+@end
