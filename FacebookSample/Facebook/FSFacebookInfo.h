adf1c9341d80334ae642fc156b2ac2fd2c86d329
akiii <akiii@sfc.wide.ad.jp>
2012-05-29 00:22:48 +0900

    Added a header.

---
+++ FacebookSample/Facebook/FSFacebookInfo.h
@@ -0,0 +1,15 @@
+//
+//  FSFacebookInfo.h
+//  FacebookSample
+//
+//  Created by 深谷 哲史 on 12/05/28.
+//  Copyright (c) 2012 Keio University. All rights reserved.
+//
+
+static NSString *kAppId = @"196702453785192";
+static NSString *kSecretKey = @"75a33e4ba6a7d20288547b40e2255875";
+
+typedef enum apiCall {
+    kAPIGraphMe,
+} apiCall;
+
