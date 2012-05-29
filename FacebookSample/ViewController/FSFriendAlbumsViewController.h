172f3bb2ff08400b9ed158a5290b8e8e8c4caf3e
akifumi <akiiiiiii@pankaku.com>
2012-05-30 01:32:10 +0900

    Added a new view controller.

---
+++ FacebookSample/ViewController/FSFriendAlbumsViewController.h
@@ -0,0 +1,15 @@
+//
+//  FSFriendAlbumsViewController.h
+//  FacebookSample
+//
+//  Created by Fukaya Akifumi on 12/05/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSFriendAlbumsViewController : UITableViewController {
+
+}
++ (FSFriendAlbumsViewController *)viewWithFriendFacebookId:(NSString *)string;
+@end
